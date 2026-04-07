5adc09d6126727c004913a55ebccbe5f7006b28a
Anjana <anj399064@gmail.com>
2026-04-07 09:06:15 +0530

    Added the db initialising file

---
+++ PaperAgent/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace PaperAgent
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ PaperAgent/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace PaperAgent
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
