281cf321146988dbe6c112c31ce1721cf4396acf
Anjana <anj399064@gmail.com>
2026-05-16 22:20:25 +0530

    Added the view model for householddetails. Have kept the db conn as well.

---
+++ PaperAgent/ViewModels/HouseholdDetailViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CommunityToolkit.Mvvm.ComponentModel;
+using PaperAgent.Services;
+
+namespace PaperAgent.ViewModels
+{
+    public partial class HouseholdDetailViewModel : ObservableObject
+    {
+        private readonly DatabaseService _dbService;
+        public HouseholdDetailViewModel(DatabaseService dbService)
+        {
+            _dbService = dbService;
+        }
+    }
+}
