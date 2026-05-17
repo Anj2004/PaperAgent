a08d1c968d82304cc064f2b963f03c8adf5127be
Anjana <anj399064@gmail.com>
2026-05-17 21:59:04 +0530

    Added householdname and householdid as observable properties(as private fields. The public property is generated by mvvm). Also added the LoadHouseholdAsync() that takes the houseold object returned from the db and stores the household name into a variable, in order to display.

--- PaperAgent/ViewModels/HouseholdDetailViewModel.cs
+++ PaperAgent/ViewModels/HouseholdDetailViewModel.cs
@@ -3,15 +3,41 @@ using System.Collections.Generic;
 using System.Text;
 using CommunityToolkit.Mvvm.ComponentModel;
 using PaperAgent.Services;
+using PaperAgent.Models;
 
 namespace PaperAgent.ViewModels
 {
+
+    [QueryProperty("HouseholdId", "id")]
     public partial class HouseholdDetailViewModel : ObservableObject
     {
         private readonly DatabaseService _dbService;
+
+        [ObservableProperty]
+        private Household _household;
+
+        [ObservableProperty]
+        private int _householdId;
+
+        [ObservableProperty]
+        private string _householdName;
+
         public HouseholdDetailViewModel(DatabaseService dbService)
         {
             _dbService = dbService;
         }
+
+
+        public async Task LoadHouseholdAsync()
+        {
+            Household household = await _dbService.GetHouseholdAsync(HouseholdId);
+            HouseholdName = household.Name;
+        }
+
+        partial void OnHouseholdIdChanged(int value)//but why partial void and why value? Start from here
+        {
+            _ = LoadHouseholdAsync();
+        }
+
     }
 }
