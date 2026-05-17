ecaccf6772f44aa76d88e717d8f899ef0bbd7c67
Anjana <anj399064@gmail.com>
2026-05-17 22:01:25 +0530

    To fetch all the details of a certain household when clicked on it - via the householddetail page.

--- PaperAgent/obj/Debug/net10.0-maccatalyst/maccatalyst-x64/PaperAgent.AssemblyInfo.cs
+++ PaperAgent/obj/Debug/net10.0-maccatalyst/maccatalyst-x64/PaperAgent.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PaperAgent")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+c0116d9db10aabfb14ebf32d275e8e08c6f6ee54")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+ce1ebd8392072e700306cb04a52fb018ee4d92ef")]
 [assembly: System.Reflection.AssemblyProductAttribute("PaperAgent")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PaperAgent")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
