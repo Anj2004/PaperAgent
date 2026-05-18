287dad56ec417152e752816fc37bf25d0fbfa5d8
Anjana <anj399064@gmail.com>
2026-05-18 22:25:39 +0530

    Added a binding context in the constructor that takes householddetail obj as paramenter. Added OnAppearing() lifecycle method that runs when the page appears

--- PaperAgent/Views/HouseholdDetailPage.xaml.cs
+++ PaperAgent/Views/HouseholdDetailPage.xaml.cs
@@ -1,9 +1,20 @@
+using PaperAgent.ViewModels;
+
 namespace PaperAgent.Views;
 
 public partial class HouseholdDetailPage : ContentPage
 {
-	public HouseholdDetailPage()
+	private readonly HouseholdDetailViewModel _viewModel;
+
+    public HouseholdDetailPage(HouseholdDetailViewModel viewModel)
 	{
 		InitializeComponent();
+		BindingContext = viewModel;
+		_viewModel = viewModel;
+	}
+
+	protected override async void OnAppearing()
+	{
+		base.OnAppearing();
 	}
 }
