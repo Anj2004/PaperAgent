24be264020276edb85f21a6b71d09c60aba3db57
Anjana <anj399064@gmail.com>
2026-05-20 22:18:30 +0530

    Added LoadSubscriptionsAsync, to show the subscription details in the household details page.

--- PaperAgent/ViewModels/HouseholdDetailViewModel.cs
+++ PaperAgent/ViewModels/HouseholdDetailViewModel.cs
@@ -4,6 +4,7 @@ using System.Text;
 using CommunityToolkit.Mvvm.ComponentModel;
 using PaperAgent.Services;
 using PaperAgent.Models;
+using System.Collections.ObjectModel;
 
 namespace PaperAgent.ViewModels
 {
@@ -25,6 +26,8 @@ namespace PaperAgent.ViewModels
         [ObservableProperty]
         private string _householdAddress;
 
+        public ObservableCollection<Subscription> Subscriptions { get; set; } = new();
+
         public HouseholdDetailViewModel(DatabaseService dbService)
         {
             _dbService = dbService;
@@ -38,12 +41,28 @@ namespace PaperAgent.ViewModels
             Household household = await _dbService.GetHouseholdAsync(HouseholdId);
             HouseholdName = household.Name;
             HouseholdAddress = household.Address;
+            await LoadSubscriptionsAsync();
         }
 
         partial void OnHouseholdIdChanged(int value)//The other half is auto generated. It returns the id as "value"
         {
             _ = LoadHouseholdAsync();//Loadhousehold returns a task. But we dont want to await for it. Its fire & forget. Need to check why.
         }
+
+        public async Task LoadSubscriptionsAsync()
+        {
+            var subscriptions = await _dbService.GetSubscriptionsAsync(HouseholdId);
+
+            MainThread.BeginInvokeOnMainThread(() =>
+            {
+                Subscriptions.Clear();
+                foreach (var subscription in subscriptions)
+                {
+                    Subscriptions.Add(subscription);
+                }
+            });
+        }
+
 
     }
 }
---
+++ PaperAgent/ViewModels/PublicationsPageViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CommunityToolkit.Mvvm.ComponentModel;
+using PaperAgent.Services;
+using System.Collections.ObjectModel;
+using PaperAgent.Models;
+using CommunityToolkit.Mvvm.Input;
+
+namespace PaperAgent.ViewModels
+{
+    public partial class PublicationsPageViewModel: ObservableObject
+    {
+        private readonly DatabaseService _dbService;
+
+        public ObservableCollection<Publication> Publications { get; set; } = new();
+
+        [ObservableProperty] 
+        private string _newName;
+
+        [ObservableProperty]
+        private string __newType;
+
+        [ObservableProperty]
+        private string _newFrequency;
+
+        [ObservableProperty]
+        private string _newPricePerIssue;
+        public PublicationsPageViewModel(DatabaseService dbService)
+        {
+            _dbService = dbService;
+        }
+
+        public async Task LoadPublicationsAsync()
+        {
+            var publications = await _dbService.GetAllPublicationsAsync();
+            MainThread.BeginInvokeOnMainThread(() =>
+            {
+                Publications.Clear();
+                foreach (var publication in publications)
+                {
+                    Publications.Add(publication);
+                }
+            });
+        }
+
+        [RelayCommand]
+        public async Task AddPublicationAsync() // stopping here, need to analyse , learn & doc
+        {
+
+        }
+
+    }
+}
