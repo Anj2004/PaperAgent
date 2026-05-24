17b7491bde8fc048a67dbb6936991ad5f38842a7
Anjana <anj399064@gmail.com>
2026-05-24 21:17:35 +0530

    had forgotten to add this binding context here. Still throwing this runtime exception

--- PaperAgent/Views/PublicationsPage.xaml.cs
+++ PaperAgent/Views/PublicationsPage.xaml.cs
@@ -1,9 +1,20 @@
+using PaperAgent.ViewModels;
+
 namespace PaperAgent.Views;
 
 public partial class PublicationsPage : ContentPage
 {
-	public PublicationsPage()
+	private readonly PublicationsPageViewModel _viewModel;
+	public PublicationsPage(PublicationsPageViewModel viewModel)
 	{
 		InitializeComponent();
+		BindingContext = viewModel;
+		_viewModel = viewModel;
+	}
+
+	protected override async void OnAppearing()
+	{
+		base.OnAppearing();
+		await _viewModel.LoadPublicationsAsync();
 	}
 }
