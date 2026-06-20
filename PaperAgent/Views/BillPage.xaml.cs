84bc00e43bdf5a1ed059b40a00bfffb93a99a009
Anjana <anj399064@gmail.com>
2026-06-20 21:58:47 +0530

    added binding contecxt for billin page

--- PaperAgent/Views/BillPage.xaml.cs
+++ PaperAgent/Views/BillPage.xaml.cs
@@ -1,8 +1,15 @@
-
+using PaperAgent.ViewModels;
 
 namespace PaperAgent.Views;
 
 public partial class BillPage : ContentPage
 {
+    private readonly BillPageViewModel _viewModel;
 
+    public BillPage(BillPageViewModel viewModel)
+    {
+        InitializeComponent();
+        BindingContext = viewModel;
+        _viewModel = viewModel;
+    }
 }
