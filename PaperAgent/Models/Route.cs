6f65ffe60e98861176f4f95ff30801a05c567100
Anjana <anj399064@gmail.com>
2026-08-03 21:21:12 +0530

    new route table

---
+++ PaperAgent/Models/Route.cs
@@ -0,0 +1,25 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace PaperAgent.Models
+{
+    [Table ("Route")]
+
+    public class Route
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        [NotNull]
+        public string Name { get; set; }
+
+        [NotNull]
+        public string Description { get; set; }
+
+        [NotNull]
+        public bool IsActive { get; set; }
+    }
+}
