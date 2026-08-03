75f0e2414da0fd3e863ba65e81b44d0a0e1f02f9
Anjana <anj399064@gmail.com>
2026-08-03 21:21:03 +0530

    added fkey of new route table

--- PaperAgent/Models/Household.cs
+++ PaperAgent/Models/Household.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 using SQLite;
 
@@ -10,6 +11,9 @@ namespace PaperAgent.Models
     {
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
+
+        [ForeignKey("Route"), NotNull]
+        public int RouteId { get; set; }
 
         [NotNull]
         public string Name { get; set; }
