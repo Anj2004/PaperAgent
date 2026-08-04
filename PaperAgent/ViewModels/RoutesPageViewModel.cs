b5d78e0b94157e04ca2a852228a4bc69a91ee4bc
Anjana <anj399064@gmail.com>
2026-08-04 09:56:01 +0530

    fns to add new route, load routes and to delete route

---
+++ PaperAgent/ViewModels/RoutesPageViewModel.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CommunityToolkit.Mvvm.ComponentModel;
+using PaperAgent.Services;
+using PaperAgent.Models;
+using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
+
+namespace PaperAgent.ViewModels
+{
+    public partial class RoutesPageViewModel : ObservableObject
+    {
+        public ObservableCollection<Route> Routes { get; set; } = new();
+
+        private readonly DatabaseService _dbservice;
+
+        [ObservableProperty]
+        private string _newName;
+
+        [ObservableProperty]
+        private string _newDescription;
+
+        public RoutesPageViewModel(DatabaseService dbservice)
+        {
+            _dbservice = dbservice;
+        }
+
+        public async Task LoadRoutesAsync()
+        {
+            var items = await _dbservice.GetAllRoutesAsync();
+            MainThread.BeginInvokeOnMainThread(() =>
+            {
+                Routes.Clear();
+                foreach (var item in items)
+                {
+                    Routes.Add(item);
+                }
+            });
+        }
+
+        [RelayCommand]
+        public async Task AddRoute()
+        {
+            try
+            {
+                if(string.IsNullOrWhiteSpace(NewName) || string.IsNullOrWhiteSpace(NewDescription))
+                    return;
+
+                Route newRoute = new Route
+                {
+                    Name = NewName,
+                    Description = NewDescription,
+                    IsActive = true
+                };
+                await _dbservice.SaveRouteAsync(newRoute);
+                await LoadRoutesAsync();
+            }
+            catch (Exception ex)
+            {
+                // Handle the exception (e.g., log it, show a message to the user, etc.)
+                Console.WriteLine($"Error adding route: {ex.Message}");
+            }
+        }
+
+        [RelayCommand]
+        public async Task DeleteRoute(Route route)
+        {
+            await _dbservice.DeleteRouteAsync(route);
+            await LoadRoutesAsync();
+        }
+    }
+}
